57d5e7303ff1a922eecc3893ce074a52053a570b
Anton Borodko <antonborodko@gmail.com>
2018-06-10 15:38:17 +0300

    Added retrieving wifi password from DB

--- src/main/java/com/perfecto/healthcheck/HealthcheckAkka.java
+++ src/main/java/com/perfecto/healthcheck/HealthcheckAkka.java
@@ -3,23 +3,24 @@ package com.perfecto.healthcheck;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import com.esotericsoftware.yamlbeans.YamlReader;
 import com.opencsv.CSVReader;
 import com.perfecto.healthcheck.actors.Controller;
 import com.perfecto.healthcheck.infra.HealthcheckProps;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HealthcheckAkka {
     public static final ActorSystem system = ActorSystem.create("healthcheck");
     public static final ActorRef controller = system.actorOf(Props.create(Controller.class),Controller.class.getName());
 
+    private static final File wifiCredentialsDbFile = new File("radius_4.yaml");
     private static final File mcmCredentialsDbFile = new File("mcm_credentials_db.csv");
 
     private static List<String[]> mcmCredentials = new ArrayList<String[]>();
+    private static Map<String,Map<String,ArrayList<Map<String,String>>>> wifiCredentialsMap;
 
     public static void main(String[] args) {
 //        Controller.McmData mcmData = new Controller.McmData(HealthcheckProps.getPerfectoHost(),HealthcheckProps.getPerfectoUser(),HealthcheckProps.getPerfectoPassword());
@@ -33,9 +34,22 @@ public class HealthcheckAkka {
             System.exit(1);
         }
         System.out.println("...done");
+
+        System.out.println("Loading WIFI credentials DB...");
+        try {
+            YamlReader reader = new YamlReader(new FileReader(wifiCredentialsDbFile));
+            wifiCredentialsMap = ((Map<String,Map<String,ArrayList<Map<String,String>>>>)reader.read());
+        } catch (Exception e) {
+            System.out.println("Unable to load Wifi credentials DB with message " + e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("...done");
+
+
         if (HealthcheckProps.getMcmCredentials() == null)
         {
-            throw new RuntimeException("Unable to get MCM credentials");
+            System.out.println("Unable to get MCM credentials");
+            System.exit(1);
         } else {
             List<String> credentials = Arrays.asList(HealthcheckProps.getMcmCredentials().split("\\r?\\n"));
 
@@ -64,6 +78,10 @@ public class HealthcheckAkka {
                     wifiIdentity = mcmName;
                 }
 
+                if (wifiPassword.equalsIgnoreCase("null")){
+                    wifiPassword = getWifiPassword(mcmName);
+                }
+
                 Controller.McmData mcmData = new Controller.McmData(mcmName,mcmUser,mcmPass,wifiName,wifiIdentity,wifiPassword);
                 controller.tell(mcmData,ActorRef.noSender());
             }
@@ -78,6 +96,19 @@ public class HealthcheckAkka {
 
     }
 
+    private static String getWifiPassword(String mcmName) {
+        String password = null;
+        for (Map.Entry<String,ArrayList<Map<String,String>>> entry:  wifiCredentialsMap.get("Datacenter").entrySet()){
+            for (Map<String,String> map:entry.getValue()){
+                if (map.get("name").equalsIgnoreCase(mcmName)){
+                    password = map.get("password");
+                }
+            }
+        }
+        return password;
+
+    }
+
 
     private static String[] getMcmCredentialsLine(String mcmName)
     {
@@ -85,7 +116,7 @@ public class HealthcheckAkka {
             return mcmCredentials
                     .stream()
                     .filter(line->line[0]
-                            .equalsIgnoreCase(mcmName + "perfectomobile.com")
+                            .equalsIgnoreCase(mcmName + ".perfectomobile.com")
                     )
                     .collect(Collectors.toList())
                     .get(0);

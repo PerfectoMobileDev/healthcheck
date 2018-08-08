a458e2423651b6210d6839afa657067b6a75d935
Anton Borodko <antonborodko@gmail.com>
2018-08-08 14:32:13 +0300

    Removed tests runner code

--- src/main/java/com/perfecto/healthcheck/actors/TestRunner.java
+++ src/main/java/com/perfecto/healthcheck/actors/TestRunner.java
@@ -24,56 +24,6 @@ public class TestRunner extends AbstractLoggingActor {
                     HealthcheckAkka.controller.tell(new Controller.TestRunnerTimeout(),self())
                 )
                 .match(RunDrivers.class, dr-> {
-                    List<DeviceDriver> deviceDrivers = dr.getDeviceDrivers();
-                    //running the tests
-                    List<DeviceStatus> deviceStatuses =
-                            deviceDrivers
-                                    .stream()
-                                    .parallel()
-                                    .map(
-                                            deviceDriver -> {
-                                                DeviceStatus deviceStatus = null;
-                                                log().info("Running tests on device " + deviceDriver.getDevice().getDeviceID());
-
-                                                try {
-                                                    if (deviceDriver.getDevice().getPlatform().equalsIgnoreCase("ios")){
-                                                        String cap = Utils.handsetInfo(deviceDriver.getDriver(),"property", "osVersion");
-                                                        String[] cap1 = cap.split("\\.");
-                                                        String newcap= cap1[0];
-                                                        Integer OS = Integer.parseInt(newcap);
-                                                        if (OS > 8){
-                                                            deviceStatus = new IOSTestSet(deviceDriver.getDriver(), deviceDriver.getDevice(), HealthcheckProps.getUUID()).runTests();
-                                                        }
-                                                        else{
-                                                            deviceStatus = new DeviceStatus(true,false," iOS OS under 9",new ArrayList<String>(),new ArrayList<String>(), deviceDriver.getDevice());
-                                                            log().info("Device " + deviceDriver.getDevice() + " iOS OS under 9");
-                                                        }
-                                                    } else {
-                                                        String cap = Utils.handsetInfo(deviceDriver.getDriver(),"property", "osVersion");
-                                                        String[] cap1 = cap.split("\\.");
-                                                        String newcap= cap1[0];
-                                                        Integer OS = 0;
-                                                        try{
-                                                            OS = Integer.parseInt(newcap);
-                                                        } catch (Exception e){
-                                                            OS=10;
-                                                        }
-                                                        if(OS > 4) {
-                                                            deviceStatus = new AndroidTestSet(deviceDriver.getDriver(), deviceDriver.getDevice(), HealthcheckProps.getUUID()).runTests();
-                                                        }else{
-                                                            deviceStatus = new DeviceStatus(true,false,"android OS under 5",new ArrayList<String>(),new ArrayList<String>(),deviceDriver.getDevice());
-                                                            log().info("Device " + deviceDriver.getDevice() + " android OS under 5");
-                                                        }
-                                                    }
-                                                } catch (Exception e) {
-                                                    log().info("Unable to process device with message " + e.getMessage());
-                                                    deviceStatus = new DeviceStatus(true,false,"Error with message " + e.getMessage(),new ArrayList<String>(),new ArrayList<String>(),deviceDriver.getDevice());
-                                                }
-                                                return deviceStatus;
-                                            }
-                                    )
-                                    .filter(Objects::nonNull)
-                                    .collect(Collectors.toList());
                    sender().tell(new Controller.DriversAfterRun(dr.getDeviceDrivers()),self());
                 })
         .build();
